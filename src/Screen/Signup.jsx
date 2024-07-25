082180a2765a81ce1a38716731eaf0f592362b78
macn1 <athulraihan27@gmail.com>
2024-07-25 22:23:40 +0530

    client side

--- src/Screen/Signup.jsx
+++ src/Screen/Signup.jsx
@@ -8,6 +8,8 @@ import './signup.css'
 
 function Signup() {
   const navigate =useNavigate()
+
+  const [err,setErr]= useState('')
   const [form,setForm]= useState({firstName:'',lastName:'',email:'',password:'',confirmPassword:''})
 
   const changeHandler =(e)=>{
@@ -16,20 +18,38 @@ function Signup() {
     setForm({...form,[name]:value})
   }
 
+
   const handleSubmit =async(e)=>{
+
+    console.log(err,"error");
     e.preventDefault()
 
-    const res = await axios.post('http://localhost:4000/user/signup',{firstName:form.firstName,lastName:form.lastName,email:form.email,password:form.password})
+    if (form.password === form.confirmPassword) {
+
+      const res = await axios.post('http://localhost:4000/user/signup',{firstName:form.firstName,lastName:form.lastName,email:form.email,password:form.password})
 
     if(res.data.status){
       navigate('/login')
     }
+    
    
+      
+    }
+    else{
+      setErr("Password doesn't match")
+   
+       
+      }
+
+    
    
   }
   return (
     <div>
         <div className='container1'>
+          <div className='center'>
+
+          
         <div className='fs-2' style={{alignItems:'start'}}>
            <h2>Signup</h2></div> 
         <div className='form'>
@@ -43,10 +63,15 @@ function Signup() {
                 <p>Already have an account ? <a href="/login">Login</a></p>
                 <button className='input2 btn btn-primary'>Login with Google</button>
 
+                <p className='bg-danger fs-4 mt-3 'style={{color:'white', marginRight:'5%'}}>{err}</p>
+
             </form>
 
         </div>
         </div>
+        </div>
+
+      
    
     </div>
     
